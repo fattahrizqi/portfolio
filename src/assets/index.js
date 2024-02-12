dda61acc10ba8b7de1c34117c2ebb1d3b1cacd20
fattahrizqi <fattah.rizqi07@gmail.com>
2024-02-12 22:19:49 +0700

    change: image extension to webp

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,7 +1,7 @@
 import logo from "./logo.svg";
-import caltara from "./caltara.png";
-import rubisold from "./rubisold.png";
-import fattah from "./fattah.png";
-import education from "./education.jpg";
+import caltara from "./caltara.webp";
+import rubisold from "./rubisold.webp";
+import fattah from "./fattah.webp";
+import education from "./education.webp";
 
 export { logo, caltara, rubisold, fattah, education };
